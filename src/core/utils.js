625593d94d7f05e64c3529497503647364990763
natdanaisu <natdanai.coe@hotmail.com>
2025-02-14 16:08:37 +0700

    update tal

--- src/core/utils.js
+++ src/core/utils.js
@@ -24,3 +24,35 @@ export const nameValidator = (name) => {
 
   return "";
 };
+
+export const getDatetext = (xDate, xLang, xSize) => {
+  if (xDate) {
+      var iDate = new Date(xDate);
+      var y1 = 0;
+      var m1 = iDate.getMonth();
+      var d1 = iDate.getDate() + "";
+      var A1 = [];
+      if (xLang == "th") {
+          if (xSize == "l") {
+              A1 = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
+          }
+          else if (xSize == "s") {
+              A1 = ["ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค", "พ.ย.", "ธ.ค."];
+          }
+          y1 = (iDate.getFullYear() + 543) + "";
+      }
+      else if (xLang == "en") {
+          if (xSize == "l") {
+              A1 = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+          }
+          else if (xSize == "s") {
+              A1 = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+          }
+          y1 = (iDate.getFullYear()) + "";
+      }
+      return d1 + " " + A1[m1] + " " + y1;
+      //return iDate;
+  }
+  
+  return "";
+};
