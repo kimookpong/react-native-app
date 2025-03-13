6a94b39b86b07c0f5d5034b5ea1b7c009719ac57
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2025-03-13 18:01:42 +0700

    update

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { AppRegistry } from "react-native";
-import { AuthProvider } from "./src/context/AuthContext";
-import AppNavigator from "./src/navigation/AppNavigator";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <AppNavigator />
-    </AuthProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-AppRegistry.registerComponent("main", () => App);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
